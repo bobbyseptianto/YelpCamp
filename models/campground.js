943744606e399ba8798152874c5f5e95998152b6
bobbyseptianto <bobbyseptianto@gmail.com>
2021-01-20 00:57:07 +0700

    Initial Commit

---
+++ models/campground.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const CampgroundSchema = new Schema({
+  title: String,
+  price: Number,
+  image: String,
+  description: String,
+  location: String,
+});
+
+const Campground = mongoose.model("Campground", CampgroundSchema);
+
+module.exports = Campground;
---
+++ seeders/index.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error"));
+db.once("open", () => {
+  console.log("Database Connected");
+});
+
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    await new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
+      price,
+    }).save();
+  }
+};
+
+seedDB().then(() => {
+  db.close();
+});
